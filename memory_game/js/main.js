ca159667fd5520003d62da3c4f62a82ee356075a
Andres Ortiz <f.andresort@gmail.com>
2018-11-27 15:12:50 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,21 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
+cards = ["queen", "queen", "king", "king"];
+cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardsInPlay[0]);
+
+
+cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again");
+	}
+}
